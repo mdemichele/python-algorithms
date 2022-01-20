43b152a4a3ed52b10d9033b38773687c43e2db9c
Matthew DeMichele <Matt@Matthews-Air.lan>
2022-01-19 18:30:21 -1000

    added some classic dynamic programming algos

---
+++ dynamic_programming/make_change/make_change.md
@@ -0,0 +1,7 @@
+# Make Change Problem 
+The basic form of the problem looks something like this: 
+
+"You're given the coins [1,2,5]. You need to return the fewest number of coins needed to add up to the given amount of 11. If the coins can't add up to 11, return -1."
+
+# Pseudocode 
+
---
+++ dynamic_programming/make_change/make_change.py
@@ -0,0 +1,70 @@
+import sys
+
+def make_change_helper(coins, amount, memo):
+    
+    print("------ Iteration ------")
+    print("amount: {}".format(amount))
+    
+    # ---------------- BASE CASES ------------------- #
+    # BASE CASE 1: If the current amount is less than 1, you've gone too far. This branch on the recursion tree is not a viable solution, so don't count it 
+    if amount < 0:
+        return -1
+        
+    # BASE CASE 2: If the current amount equals 0, you've reached a viable solution. Return a zero.
+    if amount == 0:
+        return 0
+    
+    # BASE CASE 3: You reached the current amount, so it should already be in the cache. 
+    if memo[amount] != 0:
+        return memo[amount]
+        
+        
+    # Set a bogus minimum value that you'll use to compare the new minimum 
+    minimum = sys.maxsize
+    
+    
+    # ---------------- RECURSIVE CASE ------------------ #
+    # RECURSIVE CASE: Call the helper function for each coin in coins 
+    for coin in coins:
+        # Call the helper function
+        result = make_change_helper(coins, amount - coin, memo) 
+        
+        # Each time you call the helper function, check the returned result.
+        # If result is greater than zero but less than your fake min value, set the minimum to the result 
+        if result >= 0 and result < minimum:
+            minimum = 1 + result 
+        
+        # if no miminum change number is found
+        if minimum == sys.maxsize:
+            memo[amount] = -1
+        else:
+            memo[amount] = minimum 
+    
+    print("memo: {}".format(memo))
+    # Return the cache 
+    return memo[amount]
+
+def make_change(coins, amount):
+    """Takes coins array and amount and returns the minimum number of coins to reach the amount"""
+    
+    # EDGE CASE 1: If amount is less than 1, there should be no solutions 
+    if amount < 1:
+        return 0
+        
+    # Create our memoization cache (array of 0s)
+    memo = [0] * (amount + 1)
+    
+    # Call our recursive helper function 
+    return make_change_helper(coins, amount, memo)
+    
+
+# Testing 
+if __name__ == "__main__":
+    
+    print("Testing Make Change")
+    
+    print("------- TEST 1 --------")
+    coins1 = [1, 2, 5]
+    amount = 11
+    answer = make_change(coins1, amount)
+    print("Answer should be 3: answer was {}".format(answer))
